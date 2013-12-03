142a79f36e82d034118ec7311c1bd47908a5794b
Michele <micheleguido@gmail.com>
2013-12-03 17:23:40 +0100

    working with K and C independently

--- matt.js
+++ matt.js
@@ -1,9 +1,7 @@
-//Desired parameters - pick 2 of 3, get answer for third
-
+	//Desired parameters - pick 2 of 3, get answer for third
 var log10 = function (x) {
 	return Math.log(x) / Math.log(10);
 }
-
 
 	//Antoine Coefficients
 var a_ant=5.402;
@@ -13,42 +11,52 @@ var c_ant=-31.7;
 	//Shift °C to K
 CtoK=273.18;
 
+	//listens for changes in radio buttons
+radioButtons().forEach(function (radioButton) {
+	radioButton.addEventListener("change", function(e){
+		solveFor(e.target.value);
+	});
+});
 
 
-var humidity = function (sampTemp, bathTemp) {
-    	sampTempE=sampTemp+CtoK;
-    	bathTempE=bathTemp+CtoK;
-    	return(100 * Math.pow(10, (-b_ant/(bathTempE+c_ant)+b_ant/(sampTempE+c_ant))));
+//turn nodelist into array so we can use .forEach
+function radioButtons () {
+	return([].slice.call(document.getElementsByName("unknown")));
+};
+
+//adds the class to form based on radio button selection by removing classes of all
+//and adding back the class of the one selected --we can use this to hide/show
+//input fields based on what we are solving for in CSS
+function names () {
+	return [ "humidity"
+			, "sampletemperature"
+			, "bathtemperature"
+	]
+};
+
+function solveFor (name) {
+	names().forEach(function (other){
+		document.forms[0].classList.remove(other);
+	})
+	document.forms[0].classList.add(name);
+	solve();
 }
 
-var sampleTemperature = function(relHum, bathTemp) {
-    	relHumE=relHum/100;
-    	bathTempE=bathTemp+CtoK;
-    	return(-c_ant+b_ant/(b_ant/(bathTempE+c_ant)+(log10(relHumE)))-CtoK);
+function solvingFor () {
+	return document.forms[0].classList[0];
 }
 
-var bathTemperature = function(relHum, sampTemp){
-    	relHumE=relHum/100;
-    	sampTempE=sampTemp+CtoK;
-    	return(-c_ant+b_ant/(b_ant/(sampTempE+c_ant)-(log10(relHumE)))-CtoK);
-}
 
-//tests to see if equations work with example data provided by matt
-
-var mustEqual = function(a, b, testName) {
-	if (a == b) {
-		console.log("works: " + testName)
-	} else {
-		console.log("doesn't work: " + testName)
-		console.log("  ", a, "doesn't equal", b)
+function solve () {
+	if (solvingFor() == 'humidity') {
+		computeHumidity()
+	} else if (solvingFor() == 'sampletemperature') {
+		computeSampleTemperature()
+	} else if (solvingFor() == 'bathtemperature') {
+		computeBathTemperature()
 	}
 }
 
-test = function () {
-	mustEqual(parseFloat(humidity(25, 10.4694)).toFixed(1), 40, "humidity test");
-	mustEqual(parseFloat(sampleTemperature(75, 15)).toFixed(4), 19.5491, "sample temperature test");
-	mustEqual(parseFloat(bathTemperature(82.6, 18)).toFixed(1), 15, "sample bath temperature test");
-}
 
 //changes the user-inputted values into numbers from strings, also
 //adds alert to user if it appears they inputted humidity in decimal instead of percent
@@ -70,53 +78,72 @@ function inputValueBathTemperature() {
 	return(parseFloat(document.getElementById("bathtemperature").value));
 }
 
+function inputValueSampleTemperatureK() {
+	return(parseFloat(document.getElementById("sampletemperatureK").value));
+}
+
+function inputValueBathTemperatureK() {
+	return(parseFloat(document.getElementById("bathtemperatureK").value));
+}
+
+
+
+//equations to solve for the answers
+var humidity = function (sampTemp, bathTemp) {
+    	return(100 * Math.pow(10, (-b_ant/(bathTemp+c_ant)+b_ant/(sampTemp+c_ant))));
+}
+
+var sampleTemperature = function(relHum, bathTemp) {
+    	relHumE=relHum/100;
+    	return(-c_ant+b_ant/(b_ant/(bathTemp+c_ant)+(log10(relHumE)))-CtoK);
+}
+
+var bathTemperature = function(relHum, sampTemp){
+    	relHumE=relHum/100;
+    	return(-c_ant+b_ant/(b_ant/(sampTemp+c_ant)-(log10(relHumE)))-CtoK);
+}
 
 //uses the inputted values to calculate answers, round to nearest hundredth
 //and insert them into the appropriate solution div
-
-
 function computeHumidity() {
-	//add here and then add style + to .innerHTML
-	// var style = '<style="color: red;">'
-	// var endStyle = '</style>'
-	var solution = parseFloat(humidity(inputValueSampleTemperature(), inputValueBathTemperature())).toFixed(2);
-	if (solution != "NaN") {
-	document.getElementById('humiditysolution').innerHTML = solution + " %";
-	document.getElementById('humidity').value = solution;
+	var solutionC = parseFloat(humidity((inputValueSampleTemperature()+CtoK), (inputValueBathTemperature()+CtoK))).toFixed(2);
+	var solutionK = parseFloat(humidity(inputValueSampleTemperatureK(), inputValueBathTemperatureK())).toFixed(2);
+	if (solutionC != "NaN" && solutionK != "NaN") {
+	document.getElementById('humiditysolution').innerHTML = solutionK + " %";
+	document.getElementById('humidity').value = solutionK;
+	document.getElementById('humiditysolution').innerHTML = solutionC + " %";
+	document.getElementById('humidity').value = solutionC;
+
 	}
+
 }
 
 function computeSampleTemperature() {
-	var solution = parseFloat(sampleTemperature(inputValueHumidity(), inputValueBathTemperature())).toFixed(2);
-	if (solution != "NaN") {
-		document.getElementById('sampletemperaturesolution').innerHTML = solution + " °C";
-		document.getElementById('sampletemperature').value = solution;
+	var solutionC = parseFloat((sampleTemperature(inputValueHumidity()+CtoK), (inputValueBathTemperature()+CtoK))).toFixed(2);
+	var solutionK = parseFloat(sampleTemperature(inputValueHumidity(), inputValueBathTemperature())).toFixed(2);
+	if (solutionC != "NaN" && solutionK != "NaN") {
+		document.getElementById('sampletemperaturesolution').innerHTML = solutionC + " °C";
+		document.getElementById('sampletemperature').value = solutionC;
+		document.getElementById('sampletemperaturesolutionK').innerHTML = solutionK + " K";
+		document.getElementById('sampletemperatureK').value = solutionK;
 	}
 }
 
 function computeBathTemperature() {
-	var solution = parseFloat(bathTemperature(inputValueHumidity(), inputValueSampleTemperature())).toFixed(2);
-	if (solution != "NaN") {
-	document.getElementById('bathtemperaturesolution').innerHTML=
-		solution +" °C";
-	document.getElementById('bathtemperature').value=
-		solution;
+	var solutionC = parseFloat((bathTemperature(inputValueHumidity()+CtoK), (inputValueSampleTemperature()+CtoK))).toFixed(2);
+	var solutionK = parseFloat(sampleTemperature(inputValueHumidity(), inputValueBathTemperature())).toFixed(2);
+	if (solutionC != "NaN" && solutionK != "NaN") {
+	document.getElementById('bathtemperaturesolution').innerHTML = solutionC +" °C";
+	document.getElementById('bathtemperature').value = solutionC;
+	document.getElementById('bathtemperaturesolutionK').innerHTML = solutionK +" K";
+	document.getElementById('bathtemperatureK').value = solutionK;
 	}
 }
 
-function solvingFor () {
-	return document.forms[0].classList[0];
-}
 
-function solve () {
-	if (solvingFor() == 'humidity') {
-		computeHumidity()
-	} else if (solvingFor() == 'sampletemperature') {
-		computeSampleTemperature()
-	} else if (solvingFor() == 'bathtemperature') {
-		computeBathTemperature()
-	}
-}
+
+
+
 
 //adding event listener to know that user has changed (entered) a value into the field
 document.getElementById("sampletemperature").addEventListener("change", solve);
@@ -125,36 +152,27 @@ document.getElementById("bathtemperature").  addEventListener("change", solve);
 document.getElementById("bathtemperature").  addEventListener("keyup",  solve);
 document.getElementById("humidity").         addEventListener("change", solve);
 document.getElementById("humidity").         addEventListener("keyup",  solve);
+document.getElementById("sampletemperatureK").addEventListener("change", solve);
+document.getElementById("sampletemperatureK").addEventListener("keyup",  solve);
+document.getElementById("bathtemperatureK").  addEventListener("change", solve);
+document.getElementById("bathtemperatureK").  addEventListener("keyup",  solve);
 
 
-//turn nodelist into array so we can use .forEach
-function radioButtons () {
-	return([].slice.call(document.getElementsByName("unknown")));
-};
 
 
-function names () {
-	return [ "humidity"
-			, "sampletemperature"
-			, "bathtemperature"
-	]
-};
+//tests to see if equations work with example data provided by matt
 
-//adds the class to form based on radio button selection by removing classes of all
-//and adding back the class of the one selected --we can use this to hide/show
-//input fields based on what we are solving for in CSS
-function solveFor (name) {
-	names().forEach(function (other){
-		document.forms[0].classList.remove(other);
-	})
-	document.forms[0].classList.add(name);
-	solve();
+var mustEqual = function(a, b, testName) {
+	if (a == b) {
+		console.log("works: " + testName)
+	} else {
+		console.log("doesn't work: " + testName)
+		console.log("  ", a, "doesn't equal", b)
+	}
 }
 
-//listens for changes in radio buttons
-radioButtons().forEach(function (radioButton) {
-	radioButton.addEventListener("change", function(e){
-		solveFor(e.target.value);
-	});
-});
-
+test = function () {
+	mustEqual(parseFloat(humidity((25+CtoK), (10.4694+CtoK))).toFixed(1), 40, "humidity celsius test");
+	mustEqual(parseFloat(sampleTemperature(75, (15+CtoK))).toFixed(4), 19.5491, "sample temperature celsius test");
+	mustEqual(parseFloat(bathTemperature(82.6, (18+CtoK))).toFixed(1), 15, "sample bath temperature celsius test");
+}
